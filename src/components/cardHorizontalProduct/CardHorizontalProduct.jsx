35ec12787aa89d6fbb5456303a4ba7bc719b402a
unknown <diogosilvabianchini@gmail.com>
2024-09-17 12:55:55 -0300

    Página de produtos.

---
+++ src/components/cardHorizontalProduct/CardHorizontalProduct.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import EditIcon from '@mui/icons-material/Edit';
+import "./style.css"
+
+const CardHorizontalProduct = ({id, imgUrl, title, price}) => {
+
+  return (
+    <div className='card-horizontal-container' id={`product${id}`}>
+        <div className="row">
+          <img src={`${import.meta.env.VITE_URL_S3}/${imgUrl}`} alt="product" />
+          <h2>{title}</h2>
+        </div>
+        <span>R$ {price}</span>
+        <Link className="linkButton iconButton"><EditIcon /></Link>
+    </div>
+  )
+}
+
+export default CardHorizontalProduct
--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,9 +1,44 @@
 import './style.css'
-import InputLabel from '../../components/inputLabel/InputLabel'
+import CardHorizontalProduct from '../../components/cardHorizontalProduct/CardHorizontalProduct'
+import { useEffect, useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import { Link } from 'react-router-dom';
 
 const ProductPage = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const req = await fetch(`${import.meta.env.VITE_BACK_END_URL}/product`)
+      const res = await req.json()
+      setProducts(res.results)
+    }
+
+    getProducts()
+  },[])
+
   return (
     <div className='product-page-container'>
+      <h2>Seus produtos registrados</h2>
+      <form className="search">
+        <div className="search-button">
+          <label htmlFor="search">
+            <SearchIcon />
+          </label>
+          <input type="text" id='search' placeholder='Buscar por titulo de produto...'/>
+        </div>
+        <button className='darkButton'>Pesquisar</button>
+        <Link to="/produtos/criar" className='linkButton outLineButton'>+ Criar produto</Link>
+      </form>
+      <ul className="content">
+        {
+          products.length > 0 && products.map(product => (
+            <li key={product.id}>
+              <CardHorizontalProduct id={product.id} title={product.title} price={product.price} imgUrl={product.imgs[0]}/>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
